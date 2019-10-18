b592af03da422f51a4d9427a66ad143cf2e9d07c
Wilson Adams <hiimwilson@users.noreply.github.com>
2019-10-18 12:09:08 -0500

    Update placefig.m

--- placefig.m
+++ placefig.m
@@ -1,28 +1,28 @@
-placefig() - Automated Figure Resize and Positioning
-==========================================================
-Sick of resizing all of your figures in MATLAB when you run your scripts?
-Yeah, me too. That's exactly why I made placefig().
-
-Placefig() streamlines this hassle and saves you valuable to do what you
-do best, writing the stupid code. Simply: 
-  1) create a sample figure, 
-  2) position it in the size and location you want to output on your screen,
-  3) run placefig() in the terminal window. Placefig() will output a
-      'set(gcf) command into the terminal window that you can then 
-  4) copy and 5) paste into your script so that everytime you run your script, 
-your figure pops up sized and ready to go right where you want it. (Generally,
-pasting the command right after where you create your figure is where I
-find this works best in my personal workflow.)
-
-Alternatively, if you have a figure saved as a variable on your
-workspace, you can call that variable into placefig(var) and it will
-output the set(gcf) commmand to place in your script where you need it.
-
-Have fun with it, and modify to your hearts desire. Happy MATLABing.
-==========================================================
-Created by Wilson R Adams | 18 Oct 2019 
-Vanderbilt Biophotonics Center | Vanderbilt University
-==========================================================
+% placefig() - Automated Figure Resize and Positioning
+% ==========================================================
+% Sick of resizing all of your figures in MATLAB when you run your scripts?
+% Yeah, me too. That's exactly why I made placefig().
+% 
+% Placefig() streamlines this hassle and saves you valuable to do what you
+% do best, writing the stupid code. Simply: 
+%   1) create a sample figure, 
+%   2) position it in the size and location you want to output on your screen,
+%   3) run placefig() in the terminal window. Placefig() will output a
+%       'set(gcf) command into the terminal window that you can then 
+%   4) copy and 5) paste into your script so that everytime you run your script, 
+% your figure pops up sized and ready to go right where you want it. (Generally,
+% pasting the command right after where you create your figure is where I
+% find this works best in my personal workflow.)
+% 
+% Alternatively, if you have a figure saved as a variable on your
+% workspace, you can call that variable into placefig(var) and it will
+% output the set(gcf) commmand to place in your script where you need it.
+% 
+% Have fun with it, and modify to your hearts desire. Happy MATLABing.
+% ==========================================================
+% Created by Wilson R Adams | 18 Oct 2019 
+% Vanderbilt Biophotonics Center | Vanderbilt University
+% ==========================================================
 
 function placefig(fig)
 
